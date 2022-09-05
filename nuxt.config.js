0116433c48a4194893c29eaba6861d1d337a56a2
David Alvarez <hola@alvarezdavid.com>
2022-09-04 21:20:58 -0600

    fix: deploy static pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
+    target:  'static',
     modules: ['@nuxt/content'],
     css: ["~/assets/styles/bootstrap.scss"],
 })
