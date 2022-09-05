bc39e25dacc1d69a5b623681c4c3a0a64f128505
David Alvarez <hola@alvarezdavid.com>
2022-09-04 22:48:17 -0600

    add dist folder

---
+++ dist/_nuxt/ProseTable.03e60919.js
@@ -0,0 +1,1 @@
+import{s as r,o,v as s,r as t}from"./entry.3f7476e7.js";const a={};function n(e,c){return o(),s("table",null,[t(e.$slots,"default")])}const f=r(a,[["render",n]]);export{f as default};
---
+++ dist/_nuxt/about.70c4b239.js
@@ -0,0 +1,1 @@
+import{s as e,o,v as t,B as s}from"./entry.3f7476e7.js";const c={},n=s("h1",null,"About",-1),a=[n];function r(_,l){return o(),t("main",null,a)}const d=e(c,[["render",r]]);export{d as default};
